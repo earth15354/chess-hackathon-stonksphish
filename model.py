45e9b1a9c0f94f29e416b36f897ef7bf9093ff66
Jitpuwapat (Earth) Mokkamakkul <jmokkama@gmail.com>
2024-06-30 09:39:53 +0000

    Unclear what this is, uncommited files committed

--- model.py
+++ model.py
@@ -1,57 +1,71 @@
-import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import heuristics.heuristics_v1 as hv1
+class Residual(nn.Module):
+    """The Residual block of ResNet models."""
+    def __init__(self, outer_channels, inner_channels, use_1x1conv=False):
+        super().__init__()
+        self.conv1 = nn.Conv2d(outer_channels, inner_channels, kernel_size=3, padding=1, stride=1)
+        self.conv2 = nn.Conv2d(inner_channels, outer_channels, kernel_size=3, padding=1, stride=1)
+        if use_1x1conv:
+            self.conv3 = nn.Conv2d(outer_channels, outer_channels, kernel_size=1, stride=1)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.BatchNorm2d(inner_channels)
+        self.bn2 = nn.BatchNorm2d(outer_channels)
 
-class PositionalEncoding(nn.Module):
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        Y += X
+        return F.relu(Y)
 
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
+HEURISTICS_SIZE = 14
 
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_parameter('pe', nn.Parameter(pe, requires_grad=False))
+class Model(nn.Module):
+    """Convolutional Model"""
 
-    def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
-
-class Model(nn.Transformer):
-    """Transformer Model"""
-
-    def __init__(self, ntoken, embed_dim, nhead, nhid, nlayers, batch_first, dropout=0.5):
-        super(Model, self).__init__(d_model=embed_dim, nhead=nhead, dim_feedforward=nhid, num_encoder_layers=nlayers, batch_first=batch_first)
+    def __init__(self, ntoken, embed_dim, nlayers, dropout=0.5):
+        super().__init__()
         self.model_type = 'Transformer'
         self.src_mask = None
-        self.pos_encoder = PositionalEncoding(embed_dim, dropout)
-
-        self.input_emb = nn.Embedding(ntoken, embed_dim)
+        self.ntoken = ntoken
         self.embed_dim = embed_dim
-        self.decoder = nn.Linear(64 * embed_dim, 1, bias=False)
 
-        self.init_weights()
-
-    def init_weights(self):
-        initrange = 0.1
-        nn.init.uniform_(self.input_emb.weight, -initrange, initrange)
-        nn.init.uniform_(self.decoder.weight, -initrange, initrange)
-
-    def forward(self, inputs): # (candidate, square)
+        self.input_emb = nn.Embedding(self.ntoken, self.embed_dim)
+        self.convnet = nn.Sequential(*[Residual(self.embed_dim, 5 * self.embed_dim) for _ in range(nlayers)])
+        self.accumulator = nn.Conv2d(self.embed_dim, self.embed_dim, kernel_size=8, padding=0, stride=1)
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(self.embed_dim + HEURISTICS_SIZE, 128),
+            nn.LeakyReLU(),
+            nn.Linear(128, self.embed_dim),
+            nn.LeakyReLU(),
+            nn.Linear(self.embed_dim, 1))
+        
+        
+    def forward(self, og_inputs): # (N, 8, 8)
+        n, r, c = og_inputs.shape
+        assert r == 8 and c == r
         # print(inputs.shape)
-        inputs = inputs.flatten(1)
+        inputs = self.input_emb(og_inputs) # (N, 8, 8, D) - this is nice
         # print(inputs.shape)
-        inputs = self.input_emb(inputs) * math.sqrt(self.embed_dim) # (candidate, square, embed)
+        inputs = torch.permute(inputs, (0, 3, 1, 2))
         # print(inputs.shape)
-        inputs = self.pos_encoder(inputs) # (candidate, square, embed)
+        inputs = self.convnet(inputs)
         # print(inputs.shape)
-        encoded = self.encoder(inputs) # (candidate, square, embed)
-        # print(encoded.shape)
-        latents = torch.flatten(encoded, 1) # (candidate, square_embed)
-        # print(latents.shape)
-        scores = self.decoder(latents).flatten() # (candidate)
+        inputs = F.relu(self.accumulator(inputs).squeeze())
+        assert len(inputs.shape) == 1
+        heuristics = torch.tensor([hv1.evaluate_position(i) for i in og_inputs])
+        assert heuristics.shape == (n, HEURISTICS_SIZE)
+        assert inputs.shape == (n, self.embed_dim)
+        concattenated = torch.cat([heuristics, inputs], dim=1)
+        assert concattenated.shape == (n, HEURISTICS_SIZE + self.embed_dim)
+        # print(inputs.shape)
+        scores = self.decoder(concattenated).flatten()
+        assert scores.shape == (n,)
         # print(scores.shape)
         return scores
